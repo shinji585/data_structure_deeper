8ab0f6fe05efd0a4c6fffe1c08d01e7674a3dcda
Toji <vargascardenassamueljavier@gmail.com>
2026-04-04 14:04:40 -0500

    feat: studying this again

--- linkendlist/singlylinkedlist.py
+++ linkendlist/singlylinkedlist.py
@@ -14,6 +14,7 @@ class SinglyLinkedList(Generic[T]):
     """
     def __init__(self) -> None:
         self.__head: Optional[Node] = None
+        self.__tail: Optional[Node] = None
         self.__size = 0 
         
         
@@ -22,21 +23,56 @@ class SinglyLinkedList(Generic[T]):
         return self.__size
     
     def __iter__(self) -> Generator:
-        current = self.__head
+        current = self.__tail
         while current: 
             yield current.data
             current = current.next
+            
+    def __str__(self) -> str:
+        return f"Values({[x for x in self.__iter__()]})"
+            
+            
+    # operations of the interface
         
     def append(self,data: T) -> None: 
         newNode = Node(data=data)
         
         if self.__head is None: 
             self.__head = newNode
+            self.__tail = newNode
         else: 
-            current = self.__head
-            while current.next is not None: 
-                current = current.next
-            current.next = newNode
+            self.__head.next = newNode
+            self.__head = newNode
         
         self.__size += 1 
-    
+        
+    def remove(self, data: T) -> None: 
+        current = self.__tail
+        prev: Optional[Node] = None
+        while current: 
+            if current.data == data:
+                if prev is None: 
+                    self.__tail = current.next
+                else: 
+                    prev.next = current.next
+                
+                if current == self.__head: 
+                    self.__head = prev
+                    
+                self.__size -= 1 
+                return 
+            
+            prev = current
+            current = current.next
+            
+    def search(self,data: T) -> bool: 
+        for node in self.__iter__():
+            if data == node: 
+                return True
+        return False
+                
+    def clear(self): 
+        """ clear the entire list. """
+        self.__tail = None
+        self.__head = None
+            
