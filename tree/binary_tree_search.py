1e05ebec9c615643450889519b90f277e7569b1a
Toji <vargascardenassamueljavier@gmail.com>
2026-05-09 14:21:11 -0500

    feat: insertio,min,max methods created

--- tree/binary_tree_search.py
+++ tree/binary_tree_search.py
@@ -1,4 +1,5 @@
-from typing import TypeVar, Generic, Optional
+from typing import Generic, Optional, TypeVar
+
 from tree.tree_node import Node
 
 T = TypeVar("T")
@@ -6,22 +7,62 @@ T = TypeVar("T")
 
 class BinaryTreeSearch(Generic[T]):
     def __init__(self):
-        self.root: Optional[Node] = None
+        self.root: Optional[Node[T]] = None
 
     def find_min(self) -> Node[T]:
         if self.root is None:
-            raise ValueError("Node is empty")
+            raise ValueError("Tree is empty")
 
-        current = self.root
+        current: Node[T] = self.root
         while current.left_child is not None:
             current = current.left_child
         return current
 
     def find_max(self) -> Node[T]:
         if self.root is None:
-            raise ValueError("Node is empty")
+            raise ValueError("Tree is empty")
 
-        current = self.root
+        current: Node[T] = self.root
         while current.right_child is not None:
             current = current.right_child
         return current
+
+    def insertion(self, data: T) -> None:
+        newNode = Node(data)
+
+        if self.root is None:
+            self.root = newNode
+        else:
+            current: Node[T] = self.root
+            parent: Optional[Node[T]] = None
+            while True:
+                parent = current
+                if newNode.data < current.data:
+                    assert current.left_child
+                    current = current.left_child
+                    if current is None:
+                        parent.left_child = newNode
+                        return
+                else:
+                    assert current.right_child
+                    current = current.right_child
+                    if current is None:
+                        parent.right_child = newNode
+                        return
+
+    def __get_parent_and_node__(self, data: T) -> tuple:
+        current: Node[T] = self.root
+        parent: Optional[Node[T]] = None
+
+        while current is not None:
+            if current.data == data:
+                return parent, current
+
+            elif data < current.data:
+                parent = current
+                current = current.left_child
+            else:
+                parent = current
+                current = current.right_child
+
+        return parent, None
--- tree/tree_node.py
+++ tree/tree_node.py
@@ -6,7 +6,7 @@ T = TypeVar("T")
 
 
 class Node(Generic[T]):
-    def __init__(self, data):
-        self.data: Optional[T] = data
-        self.left_child: Optional[T] = None
-        self.right_child: Optional[T] = None
+    def __init__(self, data: T):
+        self.data: T = data
+        self.left_child: Optional[Node[T]] = None
+        self.right_child: Optional[Node[T]] = None
